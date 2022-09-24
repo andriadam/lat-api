35b22894eeb54974f60964f72ba462b190c432d9
Andri Adam <andriadam2792000@gmail.com>
2022-09-24 22:25:23 +0700

    persiapan memakai jwt

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::group(['middleware' => 'api',], function ($router) {
+
+    Route::prefix('auth')->group(function(){
+        Route::post('login', 'AuthController@login');
+        Route::post('logout', 'AuthController@logout');
+        Route::post('refresh', 'AuthController@refresh');
+        Route::post('me', 'AuthController@me');
+    })
+
+    
 });
